1005a2df267eeed60175b626d362c4962b0cf650
Alvaro <alvaro.fuentesm@sansano.usm.cl>
2021-12-14 22:01:00 -0300

    Remove deprecated content

--- broker.go
+++ broker.go
@@ -595,50 +595,5 @@ func main() {
 	for {
 	}
 
-	/*
-		server_list := []int{1, 2, 3} // Deberian ir las direcciones de los server
-
-		for listening {
-
-			fmt.Println("Esperando peticion...")
-			// ESPERAR PETICION...???
-			// llega una
-			peticion := ""
-
-			// Elegir aleatoriamente uno de los tres servidores
-			randomIndex := rand.Intn(len(server_list))
-			pick := server_list[randomIndex]
-			fmt.Println("El servidor elegido para la consulta es:", pick)
-			fmt.Println(pick)
-
-			if strings.Split(peticion, " ")[0] == "GetNumberRebels" { // Es una peticion de Leia porque es la unica que puede hacer de ese tipo
-				// MANDAR LA PETICION DE LEIA AL SERVER ELEGIDO
-				fmt.Println("a")
-				// RECIBIR LA RPTA DE LOS SERVER (y el vector)
-				// MANDARSELA A LEIA
-			} else {
-				// ENVIARLE A LOS INFORMANTES EL SERVER QUE LE TOCA, ellos se encargan de enviar por si mismos
-				fmt.Println("a")
-			}
-
-			// ---------- SEGUIR EJECUTANDO ----------
-			fmt.Println("¿Desea seguir esperando comandos?:")
-			fmt.Println("1. Si")
-			fmt.Println("2. No")
-
-			opt_exit := leer_opt()
-
-			switch opt_exit {
-			case '1':
-				fmt.Println("Se reiniciará la interfaz...")
-			case '2':
-				fmt.Println("Gracias, hasta la próxima.")
-				listening = false
-				break
-			default:
-				fmt.Println("Respuesta no valida")
-			}
-		}
-	*/
 
 }
